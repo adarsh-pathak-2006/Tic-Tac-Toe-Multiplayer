3447a6563996b147f323a7d4185424d2b942765b
duckyduck01 <madhvetipathak2006@gmail.com>
2026-08-15 23:18:19 +0530

    fixed some bug

--- config/asgi.py
+++ config/asgi.py
@@ -9,15 +9,14 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 
 import os
 
-from django.core.asgi import get_asgi_application
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
+from django.core.asgi import get_asgi_application
 django_asgi_app=get_asgi_application()
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from logic.routing import url_patterns
 from logic.middleware import JwtAuthMiddleware
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = ProtocolTypeRouter({
     'websocket': JwtAuthMiddleware(URLRouter(url_patterns)),
