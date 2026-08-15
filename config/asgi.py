77e631c6e13a30b4c47307faa3c95ba369f469b4
duckyduck01 <madhvetipathak2006@gmail.com>
2026-08-15 16:31:32 +0530

    added logic app for defining core logic of the game

--- config/asgi.py
+++ config/asgi.py
@@ -10,7 +10,13 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from logic.routing import url_patterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    'websocket':AuthMiddlewareStack(URLRouter(url_patterns)),
+    'http': get_asgi_application()
+})
