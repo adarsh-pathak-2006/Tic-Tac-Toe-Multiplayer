f379be4659470f59f70da51d35b63d137e445bf2
duckyduck01 <madhvetipathak2006@gmail.com>
2026-08-15 17:19:48 +0530

    added models and also wiired them on the admin panel

--- logic/admin.py
+++ logic/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from logic.models import Game, Score, Move
 
-# Register your models here.
+admin.site.register(Game)
+admin.site.register(Score)
+admin.site.register(Move)
--- logic/models.py
+++ logic/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from authentication.models import Profile
 
-# Create your models here.
+class Game(models.Model):
+    player1=models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='player1_in_game')
+    player2=models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='player2_in_game')
+    created_on=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"game btween {self.player1.name} and {self.player2.name}"
+
+class Score(models.Model):
+    game=models.OneToOneField(Game, on_delete=models.CASCADE, related_name='scores')
+    player1_score=models.PositiveIntegerField(default=0)
+    player2_score=models.PositiveIntegerField(default=0)
+    winner=models.CharField(choices=[('PLAYER1', 'PLAYER1'), ('PLAYER2', 'PLAYER2'), ('DRAW', 'DRAW')], default='DRAW')
+
+    def save(self,*args, **kwargs):
+        if self.player1_score > self.player2_score:
+            self.winner='PLAYER1'
+        elif self.player2_score > self.player1_score:
+            self.winner='PLAYER2'
+        else:
+            self.winner='DRAW'
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.game.__str__
+
+class Move(models.Model):
+    MOVE_CHOICES=[('H1V1','H1V1'), ('H2V1', 'H2V1'), ('H3V1', 'H3V1'), ('H1V2', 'H1V2'), ('H2V2', 'H2V2'), ('H3V2', 'H3V2'), ('H1V3', 'H1V3'), ('H2V3', 'H2V3'), ('H3V3', 'H3V3')]
+    game=models.ForeignKey(Game, on_delete=models.CASCADE, related_name='moves_played')
+    player=models.CharField(max_length=10, choices=[('PLAYER1', 'PLAYER2'), ('PLAYER2', 'PLAYER2')])
+    move=models.CharField(max_length=4, choices=MOVE_CHOICES)
+
+    def __str__(self):
+        return f"{self.player} played {self.move}"
